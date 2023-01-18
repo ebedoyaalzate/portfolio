7a1e377e2ef3813884dee9dfc1158d65c719938c
ebedoyaalzate <esteban.bedoya@wolox.co>
2023-01-17 22:35:57 -0500

    create description component

---
+++ src/components/Description/index.tsx
@@ -0,0 +1,15 @@
+function Description() {
+  return (
+    <div className="text-center p-10">
+      <h2 className="text-5xl py-2 text-teal-600">Esteban Bedoya</h2>
+      <h3 className="text-2xl py-2">Frontend developer</h3>
+      <p className="text-md py-5 leading-8 text-gray-800">
+        Systems Engineer and Software Developer with four+ years of experience
+        focused frontend web development, strong skills in knowledge sharing and
+        a passion for learning new tools and skills.
+      </p>
+    </div>
+  )
+}
+
+export default Description
